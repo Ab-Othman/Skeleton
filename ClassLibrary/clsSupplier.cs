a6cd6fd6688b3378559844c9be49d1abea5426a9
Liberty Payne-Wright <P2610018@my365.dmu.ac.uk>
2022-02-15 22:19:10 +0000

    updated week 20 due to error

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSupplier
     {
+        public bool Active { get; set; }
+        public string SupplierName { get; set; }
+        public string Email { get; set; }
+        public int SupplierID { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ShippingFromAddress { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
