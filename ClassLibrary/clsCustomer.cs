ad9f830f9bb8defb695d9c8eb350acb052cb3688
BHARGAVI Pulgam <P2609745@my365.dmu.ac.uk>
2022-02-22 19:40:35 +0000

    Created tests for each attribute in my table

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public string CustomerUserId { get; set; }
+        public string CustomerFullName { get; set; }
+        public int CustomerPhoneNumber { get; set; }
+        public string CustomerEmailId { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public bool SubscribedToReceiveMail { get; set; }
+        public DateTime CustomerAccountCreated { get; set; }
     }
 }
