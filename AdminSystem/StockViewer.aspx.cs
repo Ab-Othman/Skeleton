d6bca2d4b23dd593128d20f19953a269010b5dda
Abdurahman Othman <P2623179@my365.dmu.ac.uk>
2022-03-09 18:34:21 +0000

    Completed week 21

--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsStock AnStock = new clsStock();
+        AnStock = (clsStock)Session["AnStock"];
+        Response.Write(AnStock.PhoneNo + "<br />");
+        Response.Write(AnStock.PhoneDescription + "<br />");
+        Response.Write(AnStock.PhoneColour + "<br />");
+        Response.Write(AnStock.DateReleased + "<br />");
+        Response.Write(AnStock.Price + "<br />");
+        Response.Write(AnStock.Availability + "<br />");
     }
 }
