307664630605af9747aa70d6bd1deb4301cc4e6c
FLAiRX <32580693+FLAiRX@users.noreply.github.com>
2022-03-12 21:37:15 +0000

    Fixed issues with order data entry interface

--- AdminSystem/OrderViewer.aspx.cs
+++ AdminSystem/OrderViewer.aspx.cs
@@ -10,16 +10,12 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //new instance of clsOrder
         clsOrder AnOrder = new clsOrder();
-
+        //get data from the session object
         AnOrder = (clsOrder)Session["AnOrder"];
-
-        Response.Write(AnOrder.OrderNo);
+        //display the user id on the page
         Response.Write(AnOrder.CustomerUserId);
-        Response.Write(AnOrder.OrderDate);
-        Response.Write(AnOrder.ShippingAddress);
-        Response.Write(AnOrder.PaymentMethod);
-        Response.Write(AnOrder.PaymentReceived);
-        Response.Write(AnOrder.OrderStatus);
+
     }
 }
