5f219c5cfbc8dac1f970394c0f2ab3afbac0c93c
BHARGAVI Pulgam <P2609745@my365.dmu.ac.uk>
2022-03-24 15:40:47 +0000

    Find and validate methods are working!!!

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -103,8 +103,9 @@ namespace ClassLibrary
 
         public bool Find(int CustomerUserId)
         {
+            //create an instance of the data connection
             clsDataConnection DB = new clsDataConnection();
-
+                        
             DB.AddParameter("@CustomerUserId", CustomerUserId);
 
             DB.Execute("sproc_tblCustomer_FilterByCustomerUserId");
@@ -125,6 +126,100 @@ namespace ClassLibrary
             {
                 return false;
             }              
+        }
+
+        public string Valid(string customerFullName, string customerPhoneNumber, string customerEmailId, string customerAccountCreated, string customerAddress)
+        {
+            String Error = "";
+            DateTime DateTemp;
+            //customer full name
+            //if the customer full name is blank
+            if (customerFullName.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Customer Full Name may not be blank : ";
+            }
+            
+            //if Customer full name is greater than 30
+            if (customerFullName.Length > 30)
+            {
+                Error = Error + "The Customer Full Name must be less than 30 characters : ";
+            }
+
+            //customer phone number
+            //if the customer phone number is blank
+            if(customerPhoneNumber.Length == 0)
+            {
+
+                //record the error
+                Error = Error + "The Customer Phone Number may not be blank :";
+            }
+
+            //if the customer phone number is greater than 16
+            if(customerPhoneNumber.Length > 16)
+            {
+
+                //record the error
+                Error = Error + "The Customer Phone Number must be less than 16 characters : ";
+            }
+
+            //customer email id
+            //if the customer phone number is blank
+            if (customerEmailId.Length == 0)
+            {
+
+                //record the error
+                Error = Error + "The Customer EmailId may not be blank :";
+            }
+            
+            //if the customer phone number is greater than 16
+            if (customerEmailId.Length > 30)
+            {
+
+                //record the error
+                Error = Error + "The Cusstomer Phone Number must be less than 30 characters : ";
+            }
+
+            //copy the  customer account created value to the DateTemp variable
+            try
+            {
+                DateTemp = Convert.ToDateTime(customerAccountCreated);
+                if (DateTemp < DateTime.Now.Date)
+                {
+                    Error = Error + "The date cannot be in the past : ";
+                }
+
+                //check to see if the date is greates than today's date
+                if (DateTemp > DateTime.Now.Date)
+                {
+                    Error = Error + "The date cannot be in the future : ";
+                }
+            }
+            
+            catch
+            {
+                Error = Error + "The date was not a valid date : ";
+            }
+
+
+            //customer address
+            //if the customer phone number is blank
+            if (customerAddress.Length == 0)
+            {
+
+                //record the error
+                Error = Error + "The Customer Address may not be blank :";
+            }
+
+            //if the customer phone number is greater than 16
+            if (customerAddress.Length > 50)
+            {
+
+                //record the error
+                Error = Error + "The Customer Address must be less than 50 characters : ";
+            }
+            //return any error messages
+            return Error;
         }
     }
 }
