087bd5dbcb5d1ae6a7b923ecd842092868ba2b3b
BHARGAVI Pulgam <P2609745@my365.dmu.ac.uk>
2022-03-29 17:15:49 +0100

    created collection class(properties)!

--- AdminSystem/CustomerList.aspx.cs
+++ AdminSystem/CustomerList.aspx.cs
@@ -4,11 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayCustomers();
+        }
 
+
+    }
+
+    void DisplayCustomers()
+    {
+        //creatw an instance of the customers ollection
+        clsCustomerCollection Customers = new clsCustomerCollection();
+
+        lstCustomerList.DataSource = Customers.CustomerList;
+
+        //set the name of primary key
+        lstCustomerList.DataValueField = "CustomerUserId";
+
+        //set the data field to display
+        lstCustomerList.DataTextField = "CustomerFullName";
+
+        //bind the data to the list
+        lstCustomerList.DataBind();
     }
 }
