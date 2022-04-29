fcf8a5a7bab0aed5a437a2e341872b1510f8446a
Rio <P2607633@my365.dmu.ac.uk>
2022-04-29 23:40:29 +0100

    created properties for all attributes

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -2,5 +2,12 @@
 {
     public class clsStaff
     {
+        public bool Active { get; set; }
+        public string StaffId { get; set; }
+        public string StaffFirstName { get; set; }
+        public string StaffLastName { get; set; }
+        public string StaffEmail { get; set; }
+        public int StaffPhoneNumber { get; set; }
+        public int StaffWeeklyContractedHours { get; set; }
     }
 }
--- Testing2/tstStaff.cs
+++ Testing2/tstStaff.cs
@@ -15,5 +15,98 @@ namespace Testing2
             //test to see that exists
             Assert.IsNotNull(Anstaff);
         }
+
+        [TestMethod]
+        public void ManagerPropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some data
+            Boolean TestData = true;
+            //assign the data to the property
+            AnStaff.Active = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AnStaff.Active, TestData);
+        }
+
+        [TestMethod]
+        public void StaffIdPropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some data
+            string TestData = "4";
+            //assign the data to the property
+            AnStaff.StaffId = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AnStaff.StaffId, TestData);
+        }
+
+        [TestMethod]
+        public void StaffFirstNamePropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some data
+            string TestData = "Teri";
+            //assign the data to the property
+            AnStaff.StaffFirstName = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AnStaff.StaffFirstName, TestData);
+        }
+
+        [TestMethod]
+        public void StaffLastNamePropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some data
+            string TestData = "Potter";
+            //assign the data to the property
+            AnStaff.StaffLastName = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AnStaff.StaffLastName, TestData);
+        }
+
+        [TestMethod]
+        public void StaffEmailPropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some data
+            string TestData = "Harry@gmail.com";
+            //assign the data to the property
+            AnStaff.StaffEmail = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AnStaff.StaffEmail, TestData);
+        }
+
+        [TestMethod]
+        public void StaffPhoneNumberPropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some data
+            Int32 TestData = 0740676665;
+            //assign the data to the property
+            AnStaff.StaffPhoneNumber = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AnStaff.StaffPhoneNumber, TestData);
+        }
+
+        [TestMethod]
+        public void StaffWeeklyContractedHoursPropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some data
+            Int32 TestData = 90;
+            //assign the data to the property
+            AnStaff.StaffWeeklyContractedHours = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AnStaff.StaffWeeklyContractedHours, TestData);
+
+
+        }
     }
 }
