2288f8778920c34d41144057c6fff14b7247f6b0
Rio <P2607633@my365.dmu.ac.uk>
2022-05-02 05:48:41 +0100

    StaffID StaffPhoneNumber StaffWeeklycContractedHours

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -19,15 +19,17 @@ public partial class _1_DataEntry : System.Web.UI.Page
         clsStaff AnStaff = new clsStaff();
 
         //capture the StaffFirstName
+        AnStaff.StaffId = txtStaffId.Text;
         AnStaff.StaffFirstName = txtStaffFirstName.Text;
         AnStaff.StaffLastName = txtStaffLastName.Text;
         AnStaff.StaffEmail = txtStaffEmail.Text;
+        AnStaff. StaffPhoneNumber = txtStaffPhoneNumber.Text;
+        AnStaff. StaffWeeklyContractedHours = txtStaffPhoneNumber.Text;
         //store the StaffFirstName in the session object
         Session["AnStaff"] = AnStaff;
         //navigate to the viewer page
         Response.Redirect("StaffViewer.aspx");
 
-        txtStaffPhoneNumber.Text = AnStaff.StaffPhoneNumber.ToString();
 
     }
 }
